d3215920e38a9ca9a966d74ac65dd04ace9586e2
Nick <nmarko6@gmail.com>
2020-12-18 20:38:22 -0500

    fix

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ const exjwt = require('express-jwt');
 const bodyParser  =require('body-parser');
 const path = require('path');
 
+const app = express();
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
     next();
 });
 
-const port = process.env.port || 3000;
-const app = express();
 
 var connection = mysql.createConnection({
     host : 'sql9.freemysqlhosting.net',
@@ -52,6 +51,16 @@ app.get('/api/gbudget', async (req, res) => {
         res.json(results);
     });
 });
+
+app.get('/api/login', async (req, res) => {
+    const {username, password} = res.body;
+    connection.connect();
+    connection.query('INSERT INTO user VALUES ("", ?, ?)', [username, password], function (error, results, fields) {
+        connection.end();
+        if (error) throw error;
+        res.json({success: true});
+    });
+}); 
 
 app.listen(port, () => {
     console.log(`Server on port ${port}`);
